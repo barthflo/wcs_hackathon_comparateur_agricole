41274847148dab40d9bd454eb4e681d00695284e
barthflo <flrnt.barth@gmail.com>
2021-01-13 17:34:29 +0100

    finished import csv

--- server/src/app.js
+++ server/src/app.js
@@ -4,9 +4,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 const router = require('./routes/index.routes');
-const importCsv = require('./importCsv');
-
-importCsv();
 
 app.use(cors());
 app.use('/api', router);
