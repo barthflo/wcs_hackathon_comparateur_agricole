6af370023f744e181a762a3dbe21cdb2177238a9
FlorianBEME <florianbme@gmail.com>
2021-01-14 02:40:12 +0100

    add display marker

--- server/src/routes/index.routes.js
+++ server/src/routes/index.routes.js
@@ -6,6 +6,7 @@ const buyerRouter = require('./buyer.routes.js')
 const productRouter = require('./product.routes.js')
 const transactionRouter = require('./transaction.routes.js')
 const farmerCityRouter =require ('./farmercity.routes')
+const buyerscityRouter =require ('./buyerscity.routes')
 
 router.use('/farmers', farmerRouter);
 router.use('/buyers', buyerRouter);
@@ -13,6 +14,8 @@ router.use('/cities', cityRouter);
 router.use('/products', productRouter);
 router.use('/transactions', transactionRouter);
 router.use('/farmercity', farmerCityRouter);
+router.use('/buyerscity', buyerscityRouter);
+
 
 
 module.exports = router;
