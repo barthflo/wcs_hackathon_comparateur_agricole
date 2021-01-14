f0b172b0e702ade4917e3fab168d9ddf989423e4
FlorianBEME <florianbme@gmail.com>
2021-01-14 01:04:44 +0100

    route

--- server/src/routes/index.routes.js
+++ server/src/routes/index.routes.js
@@ -5,11 +5,14 @@ const cityRouter = require('./city.routes.js')
 const buyerRouter = require('./buyer.routes.js')
 const productRouter = require('./product.routes.js')
 const transactionRouter = require('./transaction.routes.js')
+const farmerCityRouter =require ('./farmercity.routes')
 
 router.use('/farmers', farmerRouter);
 router.use('/buyers', buyerRouter);
 router.use('/cities', cityRouter);
 router.use('/products', productRouter);
 router.use('/transactions', transactionRouter);
+router.use('/farmercity', farmerCityRouter);
+
 
 module.exports = router;
